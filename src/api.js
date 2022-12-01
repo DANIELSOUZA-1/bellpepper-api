8f66fe88f166c43b783faf816bc7434b858292bf
Denes Antonio de Souza <danieldev06@gmail.com>
2022-12-01 18:53:17 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -6,7 +6,18 @@ const app = express()
 
 const router = express.Router()
 
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept',
+}
+
 router.get('/', (req, res) => {
+  res.set({
+    'Content-Type': 'application/json',
+    "access-control-allow-origin": "*"
+  }
+  )
   res.json({
     'hello': 'hi!'
   })
